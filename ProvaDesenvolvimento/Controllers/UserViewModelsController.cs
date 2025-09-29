3ae9c5401541211db254b0a266c32f96cf75bffe
HugoCCruz <gugo.hc@gmail.com>
2025-09-29 11:15:26 -0300

    Adicionar arquivos de projeto.

---
+++ ProvaDesenvolvimento/Controllers/UserViewModelsController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProvaDesenvolvimento.Models;
+
+namespace ProvaDesenvolvimento
+{
+    public class UserViewModelsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: UserViewModels
+        public ActionResult Index()
+        {
+            return View(db.UserViewModels.ToList());
+        }
+
+        // GET: UserViewModels/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserViewModel userViewModel = db.UserViewModels.Find(id);
+            if (userViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userViewModel);
+        }
+
+        // GET: UserViewModels/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult List()
+        {
+            return Json(new { usuarios = db.UserViewModels.ToList() }, JsonRequestBehavior.AllowGet);
+        }
+
+        // POST: UserViewModels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Email,BirthDate,Password,ConfirmPassword")] UserViewModel userViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserViewModels.Add(userViewModel);
+                db.SaveChanges();
+                if (!Request.IsAjaxRequest())
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return Json(new { status = "ok", usuario = userViewModel });
+                }
+            }
+            var errors = ModelState.Where(y => y.Value.Errors.Count > 0).ToDictionary(m => m.Key, m => m.Value.Errors);// Select(x => x.Value.Errors).Where(y => y.Count > 0).ToDictionary();
+
+
+            if (!Request.IsAjaxRequest())
+            {
+                return View(userViewModel);
+            }
+            else
+            {
+                return Json(new { status = "error", errors = errors });
+            }
+        }
+
+        // GET: UserViewModels/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserViewModel userViewModel = db.UserViewModels.Find(id);
+            if (userViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userViewModel);
+        }
+
+        // POST: UserViewModels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Email,BirthDate,Password,ConfirmPassword")] UserViewModel userViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userViewModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userViewModel);
+        }
+
+        // GET: UserViewModels/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserViewModel userViewModel = db.UserViewModels.Find(id);
+            if (userViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userViewModel);
+        }
+
+        // POST: UserViewModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            UserViewModel userViewModel = db.UserViewModels.Find(id);
+            db.UserViewModels.Remove(userViewModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProvaDesenvolvimento/Models/UserViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
+
+namespace ProvaDesenvolvimento.Models
+{
+    public class UserViewModel
+    {
+        [Key]
+        [DataType(DataType.Text)]
+        public String Id { get; set; }
+        
+        [Required(ErrorMessage = "O campo '{0}' é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O campo '{0}' deve possuir, no máximo, {1} caracteres.")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Nome")]
+        public String FirstName { get; set; }
+
+        [Required(ErrorMessage = "O campo '{0}' é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O campo '{0}' deve possuir, no máximo, {1} caracteres.")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Sobrenome")]
+        public String LastName { get; set; }
+
+        [StringLength(100, ErrorMessage = "O campo '{0}' deve possuir, no máximo, {1} caracteres.")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public String Email { get; set; }
+
+        [Required(ErrorMessage = "O campo '{0}' é obrigatório.")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de nascimento")]
+        public String BirthDate { get; set; }
+
+        [Required(ErrorMessage = "O campo '{0}' é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O campo '{0}' deve possuir, no mínimo, 6 caracteres.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public String Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmação de Senha")]
+        [Compare("Password", ErrorMessage = "As senhas não coincidem")]
+        public String ConfirmPassword { get; set; }
+    }
+}
